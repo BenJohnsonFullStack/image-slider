2a7b4e13923f6d9e7b82583126e173eaf3384047
Ben Johnson <monk.sound.design@gmail.com>
2023-10-16 01:01:59 -0400

    final product

--- app/page.jsx
+++ app/page.jsx
@@ -6,10 +6,30 @@ import { useState, useEffect, useRef } from "react";
 import images from "../assets";
 
 export default function Home() {
+  const [width, setWidth] = useState(0);
+  const outerCarousel = useRef();
+
+  useEffect(() => {
+    setWidth(
+      outerCarousel.current.scrollWidth - outerCarousel.current.offsetWidth
+    );
+  }, []);
+
   return (
     <>
-      <motion.div className="carousel">
-        <motion.div className="inner-carousel"></motion.div>
+      <motion.div className="carousel" ref={outerCarousel}>
+        <motion.div
+          className="inner-carousel"
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          whileTap={{ cursor: "grabbing" }}
+        >
+          {images.map((image, index) => (
+            <motion.div className="item" key={index}>
+              <Image src={image} alt="waterfalls" />
+            </motion.div>
+          ))}
+        </motion.div>
       </motion.div>
     </>
   );
